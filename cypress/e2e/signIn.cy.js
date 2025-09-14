f3ca86d100b1f4dc75f7a49b0f55d95e3a7bb520
damian Buń <damian.bun00@gmail.com>
2025-09-14 10:05:20 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,34 @@
 /// <reference types="cypress" />
 
+const {
+  generateUser
+} = require('../support/generate');
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    const {
+      userName,
+      email,
+      password
+    } = generateUser(); 
 
+    cy.request('POST', 'https://conduit.mate.academy/api/users/', {
+      "user": {
+        "username": userName,
+        "email": email,
+        "password": password
+      }
+    }); 
+
+    cy.visit('/user/login');
+
+    cy.get('[placeholder="Email"]')
+    .type(email);
+
+    cy.get('[placeholder="Password"]')
+    .type(password);
+
+    cy.get('[class="btn btn-lg btn-primary pull-xs-right"]')
+    .click();
   });
 });
